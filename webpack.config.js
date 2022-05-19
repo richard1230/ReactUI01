7d5e6c1facb65724b9ce0a5e2a11816a42b21e76
richard Hu <yongjinhu1230@gmail.com>
2022-05-20 00:30:55 +0800

    可以启动webpack-server服务了

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    // mode: 'development',
+    mode: 'production',
     //Entry  
     entry: {
         index: './lib/index.tsx'
@@ -13,31 +13,31 @@ module.exports = {
     output: {
         //__dirname -----> current dir
         path: path.resolve(__dirname, 'dist/lib'),
-        library: 'FUI',
-        //format
+        library: 'react-ui',
+        // out format
         libraryTarget: 'umd',
     },
     module: {
         rules: [
             {
-                // \ 为转义符号
+                // \ means Escape character
                 test: /\.tsx?$/,
                 loader: 'awesome-typescript-loader'
-            },
-            {
-                test: /\.svg$/,
-                loader: 'svg-sprite-loader',
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
             }
+            // {
+            //     test: /\.svg$/,
+            //     loader: 'svg-sprite-loader',
+            // },
+            // {
+            //     test: /\.scss$/,
+            //     use: ['style-loader', 'css-loader', 'sass-loader']
+            // },
+            // {
+            //     test: /\.(png|jpg|jpeg|gif)$/,
+            //     use: [
+            //         'file-loader'
+            //     ]
+            // }
         ]
     },
 
