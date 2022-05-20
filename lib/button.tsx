04583c6aa7a52e0ca22f5b9352b5ba72e783bda1
richard Hu <yongjinhu1230@gmail.com>
2022-05-20 10:25:55 +0800

    安装react和react-dom

---
+++ lib/button.tsx
@@ -0,0 +1,8 @@
+
+function Button(){
+    return(
+        <div>
+            this is a button test
+        </div>
+    )
+}
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,5 @@
-const div = document.createElement('div');
-div.innerHTML = 'this is div';
-document.body.appendChild(div);
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+console.log(React);
