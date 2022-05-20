a8764217defd270a2266d9099c77d77fef4ec940
richard Hu <yongjinhu1230@gmail.com>
2022-05-20 10:49:30 +0800

    mode--

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');  
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     //Entry  
     entry: {
         index: './lib/index.tsx'
