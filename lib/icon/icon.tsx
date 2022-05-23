b6182f16dc6de8058c3993f8e55a94ee14b84279
richard Hu <yongjinhu1230@gmail.com>
2022-05-23 16:43:18 +0800

    给icon添加事件处理函数_demo

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -4,12 +4,13 @@ import './icon.scss'
 
 interface IconProps {
     name: string;
+    onClick:()=>void
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
 
-             <svg className='fui-icon'>
+        <svg className='fui-icon' onClick={props.onClick}>
                 <use xlinkHref={`#${props.name}`}></use>
             </svg>
 
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,12 +3,15 @@ import React from 'react'
 import Icon from './icon/icon';
 
 
+const fn = () => { 
+    console.log("this is fn ")
+}
 
 ReactDOM.render(
     <div>
-        <Icon name='wechat' />
-        <Icon name='alipay' />
-        <Icon name='qq' />
+        {/* <Icon name='wechat' />
+        <Icon name='alipay' /> */}
+        <Icon name='qq' onClick={fn}/>
     </div>,
 
     document.querySelector('#root')
