3408ab88b089060c009b1e0c5f52bb968ef1f4a5
richard Hu <yongjinhu1230@gmail.com>
2022-05-25 17:01:34 +0800

    circleci_config

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,27 @@
 module.exports = {
-    verbose: true,
-    clearMocks: false,
-    collectCoverage: false,
-    reporters: ["default"],
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    moduleDirectories: ['node_modules'],
-    globals: {
-      'ts-jest': {
-        tsConfig: 'tsconfig.test.json',
-      },
+  verbose: true,
+  clearMocks: false,
+  collectCoverage: true,
+  reporters: ["default"],
+  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleDirectories: ['node_modules'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
     },
-    moduleNameMapper: {
-      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
-      "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
-    },
-    testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
-    transform: {
-      "^.+unit\\.(js|jsx)$": "babel-jest",
-      '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    setupFilesAfterEnv: ["<rootDir>test/setupTests.js"]
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
+  },
+  testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
+  transform: {
+    "^.+unit\\.(js|jsx)$": "babel-jest",
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ["<rootDir>test/setupTests.js"]
 }
   
