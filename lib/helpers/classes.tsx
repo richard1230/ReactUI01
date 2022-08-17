e37f92beddf56858ccc57834e88087f688d1b3fc
richard Hu <yongjinhu1230@gmail.com>
2022-08-17 21:53:33 +0800

    layout-classes-refactoring

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -18,26 +18,15 @@ interface ClassToggles {
 
 
 //高阶函数
-function scopedClassMaker(prefix: string) {
- return function (name?:string | ClassToggles, options?:Options) {
-   let name2;
-   let result;
-   if(typeof name === 'string' || name === undefined){
-     name2 = name;
-     result = [prefix,name2].filter(Boolean).join('-');
-   }else {
-     name2 = Object.entries(name).filter(kv => kv[1]).map(kv => kv[0]);
-     result = name2.map(n =>
-     [prefix,n].filter(Boolean).join('-')
-     ).join(' ');
-   }
-   if (options && options.extra){
-     return [result,options && options.extra].filter(Boolean).join(' ');
-   }else {
-     return result;
-   }
- }
-}
+const scopedClassMaker =(prefix: string) =>
+  (name: string | ClassToggles, options?: Options) =>
+    Object
+      .entries(name instanceof Object ? name:{[name]:name})
+      .filter(k => k[1]!==false)
+      .map(kv => kv[0])
+      .map(name => [prefix, name]
+        .filter(Boolean).join('-'))
+      .concat(options && options.extra || [])
+      .join(' ');
 
-
-export {scopedClassMaker};
+export  {scopedClassMaker}
